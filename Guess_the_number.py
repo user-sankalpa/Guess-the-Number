cbfc33153f479ab24cdb03c963c9f784e5df409b
Sankalpa Bhatta <69565400+thecyberbreaches@users.noreply.github.com>
2020-11-11 08:55:24 +0545

    Update Guess_the_number.py

--- Guess_the_number.py
+++ Guess_the_number.py
@@ -5,7 +5,7 @@ user_input=None
 guessses=0
 while(secret_number!=user_input):
     guessses+=1
-    user_input=int(input("Guess the  number--> "))
+    user_input=int(input("Guess the number--> "))
     if secret_number==user_input:
          print("Yes, you are correct.") 
     elif secret_number>user_input:
@@ -13,4 +13,4 @@ while(secret_number!=user_input):
     else:
           print("No, You are wrong. Number is bigger than secret number.")
           
-print("You guessed it correct in "+ guessses+ " guesses")
+print(f"You guessed it correct in {guessses} guesses")
