6ffdd5e252389f54e9fe21304e6358aa67903128
Sankalpa Bhatta <69565400+thecyberbreaches@users.noreply.github.com>
2020-11-11 08:06:45 +0545

    Add files via upload

---
+++ Guess_the_number.py
@@ -0,0 +1,16 @@
+import random
+
+secret_number=random.randrange(1,100)
+user_input=None
+guessses=0
+while(secret_number!=user_input):
+    guessses+=1
+    user_input=int(input("Guess the  number--> "))
+    if secret_number==user_input:
+         print("Yes, you are correct.") 
+    elif secret_number>user_input:
+          print("No, You are wrong. Number is smaller than secret number.")
+    else:
+          print("No, You are wrong. Number is bigger than secret number.")
+          
+print(f"You guessed it correct in {guessses} guesses")
